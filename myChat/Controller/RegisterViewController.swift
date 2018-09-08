211d6731231cc8df16125dd60546b5725642d417
ErnestGod <e.god465@gmail.com>
2018-09-08 18:50:10 +0200

    Registration some users to my App

--- myChat/Controller/RegisterViewController.swift
+++ myChat/Controller/RegisterViewController.swift
@@ -7,29 +7,35 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func registeredPassed(_ sender: Any) {
+        
+        //MARK: Set up a new user on my Firebase database
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration successful !")
+                
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+            
+        }
+        
     }
-    */
+    
 
 }
